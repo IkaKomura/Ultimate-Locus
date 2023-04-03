48503f488f0b0bee38dc26712f8f9792f95549b5
IkaKomura <Ika.Komura@gmail.com>
2023-04-03 21:50:12 +0200

    fixed scanCallback function ++

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,8 @@
 // Unique device ID
 const uint8_t DEVICE_ID = 1; // Make sure to set a unique ID for each device
 
-// BLE scan duration in seconds
-const int SCAN_DURATION = 5;
-
 // BLE UUIDs
 #define SERVICE_UUID "0000ABCD-0000-1000-8000-00805F9B34FB"
-#define CHARACTERISTIC_UUID "0000ABCD-0001-1000-8000-00805F9B34FB"
 
 // Maximum number of devices
 #define MAX_DEVICES 10
@@ -34,39 +30,24 @@ void startAdvertising() {
     advData.setCompleteServices(NimBLEUUID(SERVICE_UUID));
     advData.setManufacturerData(std::string((char *)&DEVICE_ID, 1));
 
-    // Add RSSI values to the advertisement data
-    for (uint8_t i = 0; i < MAX_DEVICES; i++) {
-        if (rssiValues[DEVICE_ID][i] != 0) {
-            advData.addData(std::string((char *)&i, 1) + std::string((char *)&rssiValues[DEVICE_ID][i], 1));
-        }
-    }
-
     pAdvertising->setAdvertisementData(advData);
     pAdvertising->start();
 }
 
 // Scan callback class
-class ScanCallback : public NimBLEScanCallbacks {
-    void onResult(NimBLEAdvertisedDevice *advertisedDevice) {
+class ScanCallback : public NimBLEAdvertisedDeviceCallbacks {
+    void onResult(NimBLEAdvertisedDevice* advertisedDevice) {
         // Check if the advertised device is one of our mesh devices
-        NimBLERemoteService *pService = advertisedDevice->getServiceByUUID(SERVICE_UUID);
-        if (pService) {
-            uint8_t id = pService->getCharacteristic(CHARACTERISTIC_UUID)->readUInt8();
+        if (advertisedDevice->haveServiceUUID() && advertisedDevice->isAdvertisingService(NimBLEUUID(SERVICE_UUID))) {
+            uint8_t id = advertisedDevice->getManufacturerData()[0];
             int rssi = advertisedDevice->getRSSI();
 
             // Store the RSSI value for the detected device
             storeRSSI(id, rssi);
-
-            // Parse advertised data to get RSSI values from the detected device
-            std::string advertisedData = advertisedDevice->getManufacturerData();
-            for (size_t i = 1; i < advertisedData.size(); i += 2) {
-                uint8_t deviceId = advertisedData[i];
-                int deviceRssi = advertisedData[i + 1];
-                rssiValues[deviceId][DEVICE_ID] = deviceRssi;
-            }
         }
     }
 };
+
 
 // Print RSSI values to the Serial Monitor
 void printRSSIValues() {
@@ -99,14 +80,15 @@ void setup() {
     startAdvertising();
 }
 
+ScanCallback scanCallback;
 void loop() {
     // Start BLE scan
     NimBLEScan *pScan = NimBLEDevice::getScan();
-    pScan->setAdvertisedDeviceCallbacks(new ScanCallback());
+    pScan->setAdvertisedDeviceCallbacks(&scanCallback);
     pScan->setActiveScan(true);
     pScan->setInterval(100);
     pScan->setWindow(99);
-    pScan->start(SCAN_DURATION, nullptr);
+    pScan->start(0, nullptr); // Set the scan duration to 0 for continuous scanning
 
     // Print RSSI values to the Serial Monitor
     printRSSIValues();
