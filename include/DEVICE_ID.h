65155239cd05ca6848e4f6fed363bbe496cc68ef
IkaKomura <Ika.Komura@gmail.com>
2023-04-10 11:59:35 +0200

    Handle ID in a Header file

---
+++ include/DEVICE_ID.h
@@ -0,0 +1,8 @@
+#ifndef DEVICE_ID_H
+#define DEVICE_ID_H
+
+#include <stdint.h>
+
+extern const uint8_t DEVICE_ID;
+
+#endif /* DEVICE_ID_H */
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int tInterval = 300; // Scan interval in milliseconds
 int tWindow = 200; // Scan window in milliseconds
 
 // Unique device ID
-const uint8_t DEVICE_ID = 1; // Make sure to set a unique ID for each device
+const uint8_t DEVICE_ID = DEVICE_ID; // Make sure to set a unique ID for each device
 
 // BLE UUIDs
 #define SERVICE_UUID "0000ABCD-0000-1000-8000-00805F9B34FB"
